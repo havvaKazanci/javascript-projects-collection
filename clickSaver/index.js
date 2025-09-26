ae653f5ec8f1a9e22f11092d3a182a707be46582
havvaKazanci <h.caglakazanci@gmail.com>
2025-09-26 14:58:57 +0300

    Basic Javascript code

---
+++ clickSaver/index.js
@@ -0,0 +1,19 @@
+
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+
+
+
+function increment(){
+    count += 1
+    countEl.textContent = count
+}
+
+function save(){
+    let showEl = count + " - "
+    saveEl.textContent += showEl
+    countEl.textContent = 0
+    count = 0
+}
